c58cc7e32229e0fc8dca7b8b0c1d7057699f5a08
Brendan Madden <brendanmaddento@gmail.com>
2021-08-08 15:09:14 -0400

    got mobile version completed and javascript working

---
+++ app.js
@@ -0,0 +1,17 @@
+function showModal() {
+  let modal = document.getElementById("modal");
+  if (modal.style.display === "flex") {
+    modal.style.display = "none";
+  } else {
+    modal.style.display = "flex";
+  }
+}
+
+function hideModal() {
+  let modal = document.getElementById("modal");
+  if (modal.style.display === "flex") {
+    modal.style.display = "none";
+  } else {
+    modal.style.display = "flex";
+  }
+}
